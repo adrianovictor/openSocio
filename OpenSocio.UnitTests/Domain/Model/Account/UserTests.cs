4d08632ad9aca09e684a805dc3334a801ff2cc1d
Adriano Victor <adrianovictor@hotmail.com>
2018-06-29 09:19:36 -0300

    Adicao do projeto de testes de unidade e de integração e vinculos entre os projetos.

---
+++ OpenSocio.UnitTests/Domain/Model/Account/UserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSocio.UnitTests.Domain.Model.Account
+{
+    public class UserTests
+    {
+    }
+}
---
+++ OpenSocio.Web.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenSocio.Web.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
